dd44e1a355c16f3b6467e2779e3203a0cac36326
Doniyor <donish1979@gmail.com>
2025-07-23 00:05:59 +0300

    added ad and feedback models, added to admin panels, applied migrations

--- ads/admin.py
+++ ads/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Ad, Feedback
 
-# Register your models here.
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    list_display = ("id", "title", "price", "author", "created_at")
+    list_filter = ("id", "title", "price", "author", "created_at")
+    search_fields = ("id", "title", "price", "author", "created_at")
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    list_display = ("id", "text", "author", "ad", "created_at")
+    list_filter = ("id", "text", "author", "ad", "created_at")
+    search_fields = ("id", "text", "author", "ad", "created_at")
--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,45 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from config import settings
+
+
+class Ad(models.Model):
+    """
+    Модель объявления.
+    """
+    title = models.CharField(max_length=50, verbose_name="Название товара")
+    price = models.PositiveIntegerField(verbose_name="Цена товара", validators=[MinValueValidator(1)])
+    description = models.TextField(verbose_name="Описание товара", blank=True, null=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Автор",
+                               related_name="ads")
+    created_at = models.DateTimeField(verbose_name="Дата и время создания объявления", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Дата и время изменения объявления", auto_now=True)
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Товар: {self.title}. Цена: {self.price} р."
+
+
+class Feedback(models.Model):
+    """
+    Модель отзыва.
+    """
+    text = models.TextField(verbose_name="Текст отзыва", default="", blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="Автор",
+                               related_name="user_feedbacks")
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, verbose_name="Объявление", related_name="feedbacks")
+    created_at = models.DateTimeField(verbose_name="Дата и время создания отзыва", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Дата и время изменения отзыва", auto_now=True)
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"Отзыв от {self.author} на {self.ad.title}"
