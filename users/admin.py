0cb5af326bbb511d95728b5ea024c29fba2bf80a
Doniyor <donish1979@gmail.com>
2025-07-22 23:19:50 +0300

    added customuser model, added user to admin panel, some sets in settings file

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "email", "phone", "role")
+    list_filter = ("id", "first_name", "last_name", "role")
+    search_fields = ("id", "first_name", "last_name", "email")
--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from config.constants import USER_ROLE
+
+
+class CustomUser(AbstractUser):
+    """
+    Модель кастомного Пользователя.
+    """
+    username = None
+    first_name = models.CharField(max_length=30, verbose_name="Имя", blank=True, null=True)
+    last_name = models.CharField(max_length=30, verbose_name="Фамилия", blank=True, null=True)
+    phone = models.CharField(max_length=30, verbose_name="Номер телефона", blank=True, null=True)
+    email = models.EmailField(unique=True, verbose_name="e-mail")
+    role = models.CharField(max_length=20, choices=USER_ROLE)
+    image = models.ImageField(upload_to="users/avatars/", default="user", verbose_name="Аватар", blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+    def __str__(self):
+        return self.email
